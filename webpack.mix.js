4c22900dc0331247837c77f326d4e955c041b592
Mike Martin <hello@mike-martin.ca>
2020-03-08 01:14:15 +1100

    ⚙️

---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.postCss('resources/css/app.css', 'public/css', [
+  require('postcss-import'),
+  require('tailwindcss'),
+  require('autoprefixer'),
+  require("postcss-preset-env")({
+    stage: 1,
+    features: {
+      'focus-within-pseudo-class': false
+    }
+  }),
+])
+
+if (mix.inProduction()) {
+  mix.purgeCss({
+    content: ["./public/**/*.html"],
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+  });
+}
